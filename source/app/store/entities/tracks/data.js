b7adf51172bfe3a821f1f4af1b66b4fd4a5b51f9
mahaya <donaldmahaya@yahoo.com>
2022-09-19 16:40:30 -0400

    Removed Discovery and Workflow section
    Also reduced LongDescription

--- source/app/store/entities/tracks/data.js
+++ source/app/store/entities/tracks/data.js
@@ -45,41 +45,16 @@ const discoveryTrackDescription = ENABLE_KENDRA? kendraModeDescription:classicMo
 ;
 // Initial Data
 export default {
-  search: {
-    id: 'search',
-    title: 'Discovery',
-    subtitle: 'Search across many documents, or within a single document',
-    longDescription: discoveryTrackDescription,
-    icon: '/static/images/icon_cloud-search.svg',
-    palette: 'blue',
-  },
   redaction: {
     id: 'redaction',
     title: 'Compliance',
     subtitle: 'Redact information from a document',
     longDescription: `
-      In the compliance track, you can redact information from documents. You have the
-      capability to redact specific key-value pairs detected by Amazon Textract, entities
-      detected by Amazon Comprehend and medical entities detected by Comprehend
-      Medical. You also have the flexibility to redact specific word matches under the
-      Preview Tab.
-
-      These controls help you redact Protected Health Information (PHI) and other
-      sensitive information that may be critical to your use case. You also have the option
-      to download the redacted document before you would want to share it.
+      xyz
     `,
     icon: '/static/images/icon_redact.svg',
     palette: 'teal',
   },
-  workflow: {
-    id: 'workflow',
-    title: 'Workflow automation',
-    subtitle: 'Edit and transfer data to other tools',
-    longDescription: `
-      This track focusses on how data from different AWS services in DUS can be
-      consumed. The data extracted from DUS is available in the backend storage
-      (Amazon S3) and then it can be consumed by your downstream dependencies.
-    `,
     icon: '/static/images/icon_workflow.svg',
     palette: 'purple',
   },
